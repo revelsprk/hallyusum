726389f007d48369051faac2d281f2f1b21c70c2
revelsprk <revelsprk@gmail.com>
2025-07-09 23:22:36 +0900

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,18 @@ export default function Home() {
   return (
     <div>
       <Header />
+      <div className="md:container md:mx-auto mx-4 my-8">
+        <div className="relative">
+          <img src="diva.jpg" className="rounded-lg w-full h-[512px] object-cover object-center md:h-auto" />
+          <div className="absolute inset-0 flex items-start justify-center mt-8">
+            <input
+              type="text"
+              placeholder="Search for songs..."
+              className="placeholder:text-sm bg-white border px-4 py-2 rounded-lg outline-none shadow-md w-3/4 md:w-1/4"
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <div className="sticky top-0 bg-white/40 backdrop-blur-2xl justify-center shadow-sm h-16 flex items-center overflow-hidden">
+        <div className="z-50 sticky top-0 bg-white/75 backdrop-blur-2xl justify-center shadow-sm h-16 flex items-center overflow-hidden">
             <Link href="/"><img src="/logo.svg" alt="Logo" className="h-8 hover:md:h-9 duration-200 w-fit" /></Link>
             {/* <div className="flex items-center ml-auto">
             <div className="border rounded-md shadow-sm h-8 w-48 md:w-64 overflow-hidden flex items-center px-2">
