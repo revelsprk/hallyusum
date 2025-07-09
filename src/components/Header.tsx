093298814f8690bc710b39b8da144b20211ad5f5
revelsprk <revelsprk@gmail.com>
2025-07-09 21:49:31 +0900

    update: Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { FiSearch } from "react-icons/fi";
 export default function Header() {
     return (
         <div className="bg-white/25 backdrop-blur-3xl sticky top-0 px-4 md:px-8 h-16 flex items-center overflow-hidden">
-            <Link href="/"><Image src="/logo.svg" alt="Logo" width={100} height={100} className="h-8 hover:md:h-10 duration-200 w-full" /></Link>
+            <Link href="/"><Image src="/logo.svg" alt="Logo" width={100} height={100} className="h-8 hover:md:h-10 duration-200 w-fit" /></Link>
             <div className="flex items-center ml-auto">
             <div className="border rounded-md shadow-sm h-8 w-48 md:w-64 overflow-hidden flex items-center px-2">
                 <FiSearch className="text-gray-400 text-sm mr-2" />
