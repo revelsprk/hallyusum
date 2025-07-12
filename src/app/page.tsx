22ffbdcb9cff569f5baf14f68c7c1eb082b2c92c
revelsprk <revelsprk@gmail.com>
2025-07-12 15:13:23 +0900

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="absolute inset-0 flex flex-col items-center justify-center mb-8">
         <Image src="/logo.svg" alt="Hallyu Sum" width={100} height={100} className="w-3/4 md:w-1/4 mb-2" />
         <p className="w-3/4 md:w-1/4 mb-4 text-white/80 italic">Instantly check MV view counts and rankings, all in one place.</p>
-        <input type="text" placeholder="Who’s topping the charts?" className="placeholder:text-sm bg-white border px-4 py-2 rounded-lg outline-none shadow-md w-full md:w-1/3 mx-4 md:mx-0" />
+        <input type="text" placeholder="Who’s topping the charts?" className="placeholder:text-sm bg-white border px-4 py-2 rounded-lg outline-none shadow-md w-3/4 md:w-1/3" />
       </div>
     </div>
   );
